cbe313f2570257aa71d35564904878999c8c9c05
Benito M. Zaragozí <benizar@gmail.com>
2016-07-08 12:23:26 +0200

    Create schemas.sql

---
+++ ext/sql/schemas.sql
@@ -0,0 +1,5 @@
+-- Basic data warehouse schemas
+CREATE SCHEMA IF NOT EXISTS stage; -- stage area
+CREATE SCHEMA IF NOT EXISTS ods; --operational data store
+CREATE SCHEMA IF NOT EXISTS dms; --data marts
+CREATE SCHEMA IF NOT EXISTS logs; -- Activity registry
